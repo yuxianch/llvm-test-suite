89da3e70a67786f49254655a251f536193edf62b
Bakhvalov <denis.bakhvalov@intel.com>
2020-10-28 17:02:31 -0700

    [SYCL][ESIMD] Enable most of ESIMD tests on Windows

--- SYCL/ESIMD/kmeans/kmeans.cpp
+++ SYCL/ESIMD/kmeans/kmeans.cpp
@@ -5,8 +5,8 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// TODO enable on Windows and Level Zero
-// REQUIRES: linux && gpu && opencl
+// TODO enable on Level Zero
+// REQUIRES: gpu && opencl
 // RUN: %clangxx-esimd -fsycl %s -I%S/.. -o %t.out
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out %S/points.big.json
 // RUN: %ESIMD_RUN_PLACEHOLDER %t.out %S/points.big.json
--- SYCL/ESIMD/reduction.cpp
+++ SYCL/ESIMD/reduction.cpp
@@ -6,8 +6,8 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// TODO enable on Windows and Level Zero
-// REQUIRES: linux && gpu && opencl
+// TODO enable on Level Zero
+// REQUIRES: gpu && opencl
 // RUN: %clangxx-esimd -fsycl %s -o %t.out
 // RUN: %HOST_RUN_PLACEHOLDER %t.out
 // RUN: %ESIMD_RUN_PLACEHOLDER %t.out
