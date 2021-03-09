d1cb7b96b9cd9f9dcd3950d597b34626898bc569
vladimirlaz <vladimir.lazarev@intel.com>
2021-03-09 08:26:24 +0300

    Mark test as XFAIL
    The test start to fail on CUDA BE after intel/llvm#3294

--- SYCL/Plugin/enqueue-arg-order-image.cpp
+++ SYCL/Plugin/enqueue-arg-order-image.cpp
@@ -1,7 +1,7 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple  %s -o %t.out
 // RUN: env SYCL_PI_TRACE=2 %GPU_RUN_PLACEHOLDER %t.out %GPU_CHECK_PLACEHOLDER
 // RUN: env SYCL_PI_TRACE=2 %CPU_RUN_PLACEHOLDER %t.out %CPU_CHECK_PLACEHOLDER
-
+// XFAIL: cuda
 #include <CL/sycl.hpp>
 #include <CL/sycl/accessor.hpp>
 #include <iostream>
